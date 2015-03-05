d41d93de83e19cf82a943a4c3f9718bc9ccf54cf
ShuaiZhao <s.zhao.j@gmail.com>
2015-03-05 11:49:26 -0600

    l2

--- EDA_Course_Materials/lesson2/demystifying.R
+++ EDA_Course_Materials/lesson2/demystifying.R
@@ -276,3 +276,21 @@ library(ggplot2)
 # from the following plot, you can see that "under 18" is not in the right order. That's why we need the ordered factor
 qplot(data=reddit, x=age.range)
 qplot(data=reddit, x=income.range)
+
+
+# seeting levels of ordered factors solution
+reddit$age.range <- ordered(reddit$age.range, 
+                            levels = c("Under 18","18-24","25-34","35-44", "45-54","55-64","65 or Above"))
+qplot(data=reddit, x=age.range)
+
+
+# alternate solution
+reddit$age.range <-factor(reddit$age.range,
+                          levels = c("Under 18","18-24","25-34","35-44", "45-54","55-64","65 or Above"),
+                          ordered =T)
+qplot(data=reddit, x=age.range)
+                          
+
+
+
+
